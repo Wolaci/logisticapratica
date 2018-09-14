02b0d984f2ff051073e537f240630b0f316916ff
libna <libnarodrigues723@gmail.com>
2018-09-14 18:08:16 -0300

    Arquivos: informações em tabela

---
+++ projetinho.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Just in Time</title>
+</head>
+<body>
+	<h1>Just in Time: Sistema de Controle de Cadeia de Suprimentos</h1>
+	<p></p>
+	<table border="1">
+		<tr>
+			<td id="name">Nome</td>
+			<td id="mail">Email</td>
+			<td id="fone">Telefone</td>
+		</tr>
+		<tr>
+			<td>Wolaci José</td>
+			<td>wolacijose8@gmail.com</td>
+			<td>(81)98425-0120</td>
+		</tr>
+		<tr>
+			<td>Raul Barros</td>
+			<td>raulbarrosmr@gmail.com</td>
+			<td>(81)98767-1140</td>
+		</tr>
+		<tr>
+			<td>Libna Rodrigues</td>
+			<td>libnarodrigues723@gmail.com</td>
+			<td>(81)98858-9737</td>
+		</tr>
+		<tr>
+			<td>Adely Alexandra</td>
+			<td>adely09alexandra@gmail.com</td>
+			<td>(81)98887-3472</td>
+		</tr>
+		<tr>
+			<td>Rebeca Cristina</td>
+			<td>rebecacristina535@gmail.com</td>
+			<td>(81)99719-9685</td>
+		</tr>
+		<tr>
+			<td>José Barbosa</td>
+			<td>jrlima150305@gmail.com</td>
+			<td>(81)98350-3386</td>
+		</tr>
+
+
+	</table>
+</body>
+</html>
