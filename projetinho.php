e6a7676e340a8a9e2c4bf379c4d937bbd82175f9
jackson29 <jacksonssantos@bol.com.br>
2018-09-21 15:32:05 -0300

    Pagina da descrição do projeto.

--- projetinho.php
+++ projetinho.php
@@ -1,52 +1,19 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<link rel="stylesheet" type="text/css" href="projeto.css">
+	<link rel="stylesheet" type="text/css" href="projetinho2.css/CSS">
 	<meta charset="UTF-8">
-	<title>Just in Time</title>
+	<title>Descrição</title>
 </head>
-<body>
-	<h1>Just in Time: Sistema de Controle de Cadeia de Suprimentos</h1>
-	<p>Gerenciamento de projeto</p>
+<body  background="fotocss.jpg">
+<center><h1>Descrição</h1></center>
+	<center>
+  <div >
+	<p>O “Your Store” trabalha com a metodologia “Just in time” e consiste em ser um sistema para gerenciamento de estoque e armazenamento de materiais. Com este site será possível realizar cadastros, entradas e saídas de produtos mediantes notas fiscais que também poderão ser emitidas para maior confiabilidade. Além de classificar os produtos para facilitar sua ordem de importância no sistema. Tudo isso gerando relatórios para controles e futuras melhorias do projeto. Com uma equipe formada por: Adely Alexandra, Wolaci José, Raul Barros, Rebeca Cristina, José Barbosa e Libna Rodrigues.
+</p>
+</div>
+</center>
+<center><img src="descricao.jpg" alt="" style="width:10; height:600px;" /></center>
 
-	<p>Pequeno passo para um aluno,mas um grande passo para um projeto bem feito!!</p>
-	<table >
-		<tr class="titulo">
-			<td id="name">Nome</td>
-			<td id="mail">Email</td>
-			<td id="fone">Telefone</td>
-		</tr>
-		<tr>
-			<td>Wolaci José</td>
-			<td>wolacijose8@gmail.com</td>
-			<td>(81)98425-0120</td>
-		</tr>
-		<tr>
-			<td>Raul Barros</td>
-			<td>raulbarrosmr@gmail.com</td>
-			<td>(81)98767-1140</td>
-		</tr>
-		<tr>
-			<td>Libna Rodrigues</td>
-			<td>libnarodrigues723@gmail.com</td>
-			<td>(81)98858-9737</td>
-		</tr>
-		<tr>
-			<td>Adely Alexandra</td>
-			<td>adely09alexandra@gmail.com</td>
-			<td>(81)98887-3472</td>
-		</tr>
-		<tr>
-			<td>Rebeca Cristina</td>
-			<td>rebecacristina535@gmail.com</td>
-			<td>(81)99719-9685</td>
-		</tr>
-		<tr>
-			<td>José Barbosa</td>
-			<td>jrlima150305@gmail.com</td>
-			<td>(81)98350-3386</td>
-		</tr>
-
-	</table>
 </body>
 </html>
