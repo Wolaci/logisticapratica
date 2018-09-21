5bb84962d962ef327455d495cfdba41f77bf5dbe
Raul <raulbarrosmr@gmail.com>
2018-09-21 18:40:53 -0300

    Desenvolvendo index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Logística Prática</title>
+	<link rel="stylesheet" type="text/css" href="\css">
+</head>
+<body>
+	<h1 id=titulo>Logística Prática</h1>
+</body>
+</html>
