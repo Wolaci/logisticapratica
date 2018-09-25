611d83113d72d9744d921ccbc262dc0cdf77f0e6
jackson29 <jacksonssantos@bol.com.br>
2018-09-25 16:26:42 -0300

    Atualização 25/09/18

--- cabecalho.php
+++ cabecalho.php
@@ -5,11 +5,20 @@
 	<title>Logística Prática</title>
 	<link rel="stylesheet" type="text/css" href="index2.css">
 </head>
-<body>
-	<header>
-	<img src="imagens/logomarca.png">
-	<a href="auxilio.php"><button id="botao">Ajuda</button></a>
-	<a href="sobre.php"><button>Sobre</button></a>
-	<a href=""><button>Cadastro</button></a>
-	</header>
+	<body>
+		<div class="container">
+			<header>
+				<img src="imagens/logomarca.png">
+				<ul type="none" class="btn">
+					<a href="#"><li>Início</li></a>
+					<a href="#"><li>Cadastro</li></a>
+					<a href="#"><li>Ajuda</li></a>
+					<a href="#"><li>Sobre</li></a>
+				</ul>
+			</header>
+		</div>
+	</body>
+</html>
+
+
 	
