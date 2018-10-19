bccb61555750e5639501b7eb036fea1e0b4ade90
libna <libnarodrigues723@gmail.com>
2018-10-19 19:18:27 -0300

    introdução a POO do usuario

--- php/ajuda.php
+++ php/ajuda.php
@@ -6,6 +6,7 @@ require_once '../index.php';
 
 <div>
 	<fieldset>
+		<h1>Cadastro de materiais</h1>
 		<p>Para realizar o cadastro de produtos é necessário preencher os campos requisitados que são: o nome do produto, código do produto, validade do produto, data de chegada do produto e o cadastro do lote do produto.</p>
 	</fieldset>
 </div>
