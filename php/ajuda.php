c54b73e93f21310ca4dc6b0acbd13ba9e1ef371d
Wolaci <wolacijose8@gmail.com>
2018-10-22 16:00:50 -0300

    Botão de ajuda atualizado com auxílio no login e no registro

--- php/ajuda.php
+++ php/ajuda.php
@@ -6,7 +6,11 @@ require_once '../index.php';
 
 <div>
 	<fieldset>
-		<h1>Cadastro de materiais</h1>
-		<p>Para realizar o cadastro de produtos é necessário preencher os campos requisitados que são: o nome do produto, código do produto, validade do produto, data de chegada do produto e o cadastro do lote do produto.</p>
+		<h1>Cadastro de produtos</h1>
+		<p>Para realizar o cadastro de produtos é necessário fazer login com seu nome de usuário e senha, após fazer isso você deve preencher os campos requisitados que são: o nome do produto, código do produto, validade do produto, data de chegada do produto e o cadastro do lote do produto.</p>
+		<h1>Login</h1>
+		<p>Para fazer login é necessário inserir seu nome de usuário e a senha registrada.</p>
+		<h1>Registro</h1>
+		<p>Antes de poder logar é necessário registrar seu nome de usuário e senha, para fazer isso é necessário clicar no botão "cadastre-se" na tela de login e assim você será direcionado para a tela de registro e poderá registrar seu usuário e senha.</p>
 	</fieldset>
 </div>
