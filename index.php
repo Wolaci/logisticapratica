38462df889e9ebe506135dc5d5bad5caafb272d1
Raul <raulbarrosmr@gmail.com>
2018-10-22 17:59:22 -0300

    Novo botão para usuario e teste de carrossel

--- index.php
+++ index.php
@@ -16,9 +16,12 @@
       <div class="container-fluid">
         <img src="../../img/logos.png">
           <ul class="nav nav-tabs">
+            <li>
+            <a class="btn btn-dark" href="home.php" role="button">Home</a>
+            </li>
             <li class="nav-item">
             </li>
-              <a class="btn btn-dark" href="/php/home.php" role="button">Home</a>
+              <a class="btn btn-dark" href="/php/sobre_novo.php" role="button">Sobre</a>
             <li class="nav-item">
             </li>
               <a class="btn btn-dark" href="/php/login/login.php" role="button">Login</a>
@@ -27,10 +30,28 @@
             </li>
               <a class="btn btn-dark" href="/php/ajuda.php" role="button">Ajuda</a>
             <li>
-            	<a class="btn btn-dark" href="/php/sobre.php" role="button">Sobre</a>
+              <a class="btn btn-dark" href="/php/sobrenos.php" role="button">Quem somos nós?</a>
             </li>
+            
+
+            <li>
+              <div class="btn-group">
+                <a type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">Usuário</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Login</a>
+                <a class="dropdown-item" href="#">Cadastro</a>
+                <div class="dropdown-divider"></div>
+              </div>
+            </div>
+            </li>
+          
+          
           </ul>
       </div>
     </header>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
