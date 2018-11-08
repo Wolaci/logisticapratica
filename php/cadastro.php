1477889e746c020851b48032cc974bec72ff8483
Raul Barros <raulbarrosmr@gmail.com>
2018-11-08 16:49:19 -0300

    Atualização do corpo do cadastro de produtos

--- php/cadastro.php
+++ php/cadastro.php
@@ -19,57 +19,57 @@ require_once ('menu.php');
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="../css/css.css" />
-	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>
 
-<fieldset class="field_cadastro">
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+			<div class="produto">
+ 				<h2>Cadastro de Produtos</h2>
+					<fieldset>
+					<form action="cadprod/addNome.php" method="POST">
+						<ul>
+						<li>Cadastre o Produto</li>
+						<input type="text" name="nome" placeholder="Digite o nome do Produto">
 
- <h2>Cadastro</h2>
+						<li>Cadastre o Código do Produto</li>
+						<input type="text" name="codigo" placeholder="Digite o codigo do Produto">
 
-	<form action="cadprod/addNome.php" method="POST">
-	
-		<b>Cadastre o Produto</b>
-		<input type="text" name="nome" placeholder="Digite o nome do Produto" > 
+						<li>Validade do Produto</li>
+						<input type="date" name="validade" placeholder="dd/mm/aaaa">
 
-		<b>Cadastre o Código do Produto</b>
-		<input type="text" name="codigo" placeholder="Digite o codigo do Produto"><br>
-
-		<b>Validade do Produto</b><br>
-		<input type="date" name="validade" placeholder="dd/mm/aaaa"><br>
-
-		<br><b>Data da Chegada do Produto a Loja</b><br>
-		<input type="date" name="chegada" placeholder="dd/mm/aaaa"><br>
+						<li>Data da Chegada do Produto a Loja</li>
+						<input type="date" name="chegada" placeholder="dd/mm/aaaa">
 		
-		<br><b>Cadastre o lote do Produto</b>
-		<input type="text" name="lote" placeholder="Digite o lote">
+						<li>Cadastre o lote do Produto</li>
+						<input type="text" name="lote" placeholder="Digite o lote">
 
-		<b>Quantidade</b><br>
-		<input type="number" name="quant" placeholder="Escolha a quantidade" required>
-
-
-
-		<input class="botao_cadastro" type="submit" value="Cadastrar no Sistema">
-	</form>
-</fieldset>
-
-<fieldset class="field_estoque">
-	
-<h2>Estoque</h2>
+						<li>Quantidade</li>
+						<input type="number" name="quant" placeholder="Escolha a quantidade" required>
+			</div>
+						<input class="botao_cadastro" type="submit" value="Cadastrar no Sistema">
+						</ul>
+					</form>
+					</fieldset>
+		</div>
+	</div>
 
 
-	<table border="1" cellpadding="5" cellspacing="0">
-		<tr>
-
-			<th>Nome</th>
-			<th>Código</th>
-			<th>Validade</th>
-			<th>Chegada</th>
-			<th>Lote</th>
-			<th>Quantidade</th>
-			<th>Excluir</th>
-
-		</tr>
+<div class="row">
+		<div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">	
+			<h2>Estoque</h2>
+				<table border="1" cellpadding="5" cellspacing="0">
+					<tr>
+						<th>Nome</th>
+							<th>Código</th>
+							<th>Validade</th>
+							<th>Chegada</th>
+							<th>Lote</th>
+							<th>Quantidade</th>
+							<th>Excluir</th>
+						</tr>
 		<?php 
 		
 		
@@ -97,11 +97,14 @@ require_once ('menu.php');
 									<td><a href="/php/cadprod/rmNome.php?id=<?= $res['id'] ?>"><img src="../img/excluir.png"></excluir></a></td>
 								</tr> 
 								<?php endwhile; ?>
-	</table>
-</fieldset>	
+	</table>	
 <a href="login/logout.php"><button class="botao_sair">Sair</button></a>
-
+</div>
 </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>	
+
 </html>
 <?php
  $alert=$conn->prepare('SELECT * FROM produto_pdo WHERE fk_user = :f');
