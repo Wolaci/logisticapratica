ec09e4d06ff8d7423fb35277477c9fa201f87d20
wolaci <wolacijose8@gmail.com>
2018-11-16 17:15:14 -0300

    correção das cores do cadastro e saída de produtos

--- php/exit.php
+++ php/exit.php
@@ -14,7 +14,7 @@ $u->conectar();
 <head>
 	<title></title>
 	<style type="text/css">
-		body {font-family: Arial, Helvetica, sans-serif;}
+		body {font-family: Arial, Helvetica, sans-serif;color: white;}
 
 input[type=text], input[type=password] {
     width: 100%;
