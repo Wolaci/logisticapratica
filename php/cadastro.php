ecdd81319af9c7aceda106585554a3fa339813f3
libna <libnarodrigues723@gmail.com>
2018-11-23 17:21:57 -0300

    '

--- php/cadastro.php
+++ php/cadastro.php
@@ -31,7 +31,7 @@ require_once ('menu.php');
 					<fieldset>
 					<form action="cadprod/addNome.php" method="POST">
 						<ul>
-						<li>Cadastre o Produto</li>
+						<p>Cadastre o Produto</p>
 						<input type="text" name="nome" placeholder="Digite o nome do Produto">
 
 						<li>Cadastre o Código do Produto</li>
@@ -55,6 +55,33 @@ require_once ('menu.php');
 					</fieldset>
 		</div>
 	</div>
+
+
+<div class="modal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+	
 
 
 <div class="row">
