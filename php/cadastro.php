6922f4e20cd0b0439dc24561718fd25b3cfcb5de
Wolaci <wolacijose8@gmail.com>
2018-11-28 14:58:09 -0300

    Vendas diretas do cadastro de materiais.

--- php/cadastro.php
+++ php/cadastro.php
@@ -113,6 +113,7 @@ if (isset($_SESSION['login'])) {
 						<td><?=$res['quantidade']?></td>
 						<td><a href="/php/cadprod/rmNome.php?id=<?= $res['id'] ?>">
 						<img src="../img/excluir.png"></excluir></a></td>
+						<td><a href="/php/exit.php?id=<?= $res['id'] ?>"">oi</a></td>
 						<td><a href="/php/cadprod/altNome.php?id=<?= $res['id'] ?>"> <img src="../img/up.jpg" width="30px" height="30px"></a></td>
 						<td><?php 
 							//$alert=$conn->prepare('SELECT quantidade FROM produto_pdo WHERE fk_user = :f AND nome="$res[nome]" AND quantidade="0"');
