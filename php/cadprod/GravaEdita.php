6d2827a4ed1f7a20332b787d7656253381c9bc2e
JoseBarbosa <jrlima150305@gmail.com>
2018-11-30 20:09:37 -0300

    link de update e update do nome da do produto da tabela estoque da pagina listar produtos

--- php/cadprod/GravaEdita.php
+++ php/cadprod/GravaEdita.php
@@ -1,30 +1,16 @@
 <?php
 	session_start();
-
 	include '../POO/Usuario.php';
 	$u=new Usuario;
  	 $u->conectar();
 	//include '../POO/Produto.php';
 
-/*try {
-			$conn = new PDO("mysql:host=localhost;dbname=id7218638_logistica", 'root','junior150305');
-		} catch (PDOException $e) {
-			echo "Connection failed: " . $e->getMessage();
-		}
-	*/
-	$nomeUser=$_SESSION['login'];
+	//$nomeUser=$_SESSION['login'];
 	$nome = $_POST['nome'];
-	$codigo = $_POST['codigo'];
-	$validade = $_POST['validade'];
-	$chegada = $_POST['chegada'];
-	$lote =$_POST['lote'];
-	$id = $_POST['id'];
-
-	$ce = $conn->prepare("UPDATE produto_pdo Set nome='?' codigo= '?' validade='?' chegada='?' lote= '?' WHERE id = ? and fk_user = $nomeUser");
-
-	$ce->execute([$validade,$chegada,$lote,$nome,$codigo,$id]);
 	
+	$id = $_POST['id'];
+	$ce = $conn->prepare("UPDATE produto_pdo Set nome= ?  WHERE id = ?");
+	$ce->execute([$nome,$id]);
 
-	header('location:../cadastro.php');
-
+	header('location:../listar.php');
 ?>
--- php/cadprod/altNome.php
+++ php/cadprod/altNome.php
@@ -5,30 +5,24 @@ include '../POO/Usuario.php';
 $u = new Usuario();
 $u -> conectar();
 $nameUs=$_SESSION['login'];
-
-	$id = $_GET['id'];
-
-	/*include 'conexao.php';
-
-
-	$nomes=("SELECT * FROM nome WHERE id = $id");
-	
-	$result = $conn->query($nomes);
-	
-	$res = $result->fetch(PDO::FETCH_ASSOC);
-
-	*/			
+    $id = $_GET['serio'];
+    /*include 'conexao.php';
+    $nomes=("SELECT * FROM nome WHERE id = $id");
+    
+    $result = $conn->query($nomes);
+    
+    $res = $result->fetch(PDO::FETCH_ASSOC);
+    */          
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-	<style type="text/css">
-		 <style>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style type="text/css">
+         <style>
         body {color:white; font-family: Arial, Helvetica, sans-serif;}
-
         input[type=text], input[type=password] {
             width: 100%;
             padding: 12px 20px;
@@ -37,7 +31,6 @@ $nameUs=$_SESSION['login'];
             border: 1px solid #ccc;
             box-sizing: border-box;
         }
-
         button {
             background-color: #4CAF50;
             color: white;
@@ -47,36 +40,29 @@ $nameUs=$_SESSION['login'];
             cursor: pointer;
             width: 100%;
         }
-
         button:hover {
             opacity: 0.8;
         }
-
         .cancelbtn {
             width: auto;
             padding: 10px 18px;
             background-color: #f44336;
         }
-
         .imgcontainer {
             text-align: center;
             margin: 24px 0 12px 0;
         }
-
         img.avatar {
             width: 40%;
             border-radius: 50%;
         }
-
         .container {
             padding: 16px;
         }
-
         span.psw {
             float: right;
             padding-top: 16px;
         }
-
         /* Change styles for span and cancel button on extra small screens */
         @media screen and (max-width: 300px) {
             span.psw {
@@ -89,23 +75,17 @@ $nameUs=$_SESSION['login'];
            h1{}
        }
    </style>4
-	</style>
+    </style>
 </head>
 <body>
 <div class="container">
 <fieldset>
-	<form action="GravaEdita.php" method="post">
-		<input class="form-group" type="text" name="nome" value="<?= $res['nome'] ?>" placeholder="Produto">
-		<input type="text" name="<?=$res['codigo']?>" placeholder="codigo">
-		<input type="date" name="<?=$res['validade']?>">
-		<input type="date" name="<?=$res['chegada']?>">
-		<input type="text" name="<?=$res['lote']?>" placeholder= "lote">
-		<input type="number" name="<?=$res['quantidade']?>" placeholder = "quantidade">
-
-		<input type="hidden" name="id" value="<?= $id ?>">
-		<button type="submit">alterar</button>
-	</form>
-	</fieldset>
+    <form action="GravaEdita.php" method="post">
+        <input class="form-group" type="text" name="nome"  placeholder="Produto">
+         <input type="hidden" name="id" value="<?= $id ?>">
+        <button type="submit">alterar</button>
+    </form>
+    </fieldset>
 </div>
 </body>
 </html>
